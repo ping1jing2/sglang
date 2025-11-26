6e4cd335ab659bc39221f42f0910839f44464852
MichelleWu351 <michellewu351@gmail.com>
2025-11-26 19:11:23 +0800

    NZ cast

--- python/sglang/srt/layers/quantization/unquant.py
+++ python/sglang/srt/layers/quantization/unquant.py
@@ -20,6 +20,7 @@ from sglang.srt.utils import (
     get_bool_env_var,
     is_cpu,
     is_hip,
+    is_npu,
     set_weight_attrs,
     use_intel_amx_backend,
 )
@@ -34,6 +35,7 @@ if TYPE_CHECKING:
 _is_cpu_amx_available = cpu_has_amx_support()
 _is_hip = is_hip()
 _is_cpu = is_cpu()
+_is_npu = is_npu()
 _use_aiter = get_bool_env_var("SGLANG_USE_AITER") and _is_hip
 
 if _use_aiter:
@@ -108,7 +110,9 @@ class UnquantizedLinearMethod(LinearMethodBase):
     def process_weights_after_loading(self, layer: torch.nn.Module) -> None:
         if _is_cpu and _is_cpu_amx_available:
             _amx_process_weight_after_loading(layer, ["weight"])
-
+        if _is_npu:
+            import torch_npu
+            layer.weight.data = torch_npu.npu_format_cast(layer.weight.data, 29)
     def apply(
         self,
         layer: torch.nn.Module,
